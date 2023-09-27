bebcbeccc65e0f0579adff38a78a6d95328631fb
ZidaAdz12 <146154324+ZidaAdz12@users.noreply.github.com>
2023-09-27 11:48:48 +0700

    Add files via upload

---
+++ tugas pertemuan ke 3/main (2).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+<using namespace std;
+
+int main () {
+  int a;
+  int b = 2;
+   
+  count << "masukan bilangan
+    cin >> a;
+  
+  if (a % b == 0) {
+    cout << "bilangan yang anda input adalah bilangan genap";
+  } else {
+    cout << a << "bilangan yang anda input adalah bilangan ganjil";
+  }
+
+  cout << endl;
+  return 0;
+}
